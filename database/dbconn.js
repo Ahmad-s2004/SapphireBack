7b66b6a141ddc6f4ad178f80c3b24bf66ecaee82
Ahmad Amman <132729640+Ahmad-s2004@users.noreply.github.com>
2024-03-06 17:49:13 +0500

    Update dbconn.js

--- database/dbconn.js
+++ database/dbconn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = () =>{
     try {
-     mongoose.connect("mongodb://127.0.0.1:27017/websiteData")
+     mongoose.connect("mongodb+srv://ahmadamman:6jritwx@cluster0.gmwbecz.mongodb.net/webdata")
         console.log(`Database connected successfully`)      
     } catch (error) {
         console.log(error) 
